ed27ed218dc4aafeafcd458e332d2cb52323b878
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2026-03-12 18:42:51 +0530

    Task1 completed

--- Task3/meeting-room-api/routes/roomRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const roomController = require("../controllers/roomController");
-
-router.post("/rooms", roomController.createRoom);
-
-router.get("/rooms", roomController.getRooms);
-
-module.exports = router;
